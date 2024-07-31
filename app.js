29e0ae4db223d5bd26d129132e15d1b9b14c25d8
Noppamine2106 <waranya_char@cmu.ac.th>
2024-07-31 07:16:17 +0000

    Update7Re

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-console.log("Hello World")
-console.log("Bye World...")
-
-app.listen(9999,function(){
-console.log("Server is running on PORT 9999")
-})
